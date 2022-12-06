937d058b0bb11fe90b16be233ade35ec9891e1ea
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2022-12-06 01:15:55 -0800

    Hi Fidelity Home Page UI
    Implementing homepage. Change made in Dashboard.js, Queue.js, Track.js, and DisplayResults.js. I have used Container and Tabs.

--- client-sq/src/components/Dashboard.js
+++ client-sq/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {Container, Form} from 'react-bootstrap';
 import '../styles/App.css'
 import axios from 'axios';
-import Track from "./Track"
 import Queue from "./Queue"
-import {  TableContainer, Table, TableCell, TableBody, TableHead, TableRow, Paper } from '@mui/material';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ScheduleRoundedIcon from '@mui/icons-material/Schedule';
+import CommentRoundedIcon from '@mui/icons-material/Comment';
+import { TextField, Table, Tab, Tabs, Container, TableRow, TableContainer, tableCellClasses,} from '@mui/material';
 import DisplayResults from "./DisplayResults";
 
 function Dashboard(){
@@ -12,6 +13,12 @@ function Dashboard(){
     const [goodSongsArr, setPassArr] = useState([])
     const [search, setSearch] = useState("")
     const [queueData, setQueueData] = useState([])
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event,newValue) => {
+      setValue(newValue);
+      console.log(value)
+    };
 
     // Hook handling retrieving the data of the queue from the backend.
     useEffect(() => {
@@ -120,36 +127,81 @@ function Dashboard(){
     }, [search])
 
     return (
+    <div  style={{ display:"inline-flex", backgroundColor:"#f6f8fe", width:window.innerWidth, height:window.innerHeight}}>
+      <Container style={{
+                        backgroundColor:"#ffffff",
+                        width: window.innerWidth*0.2,
+                        height: "100vh",
+                        fontFamily:"DM Sans"
+                        }}
+                        sx={{boxShadow:10}}
+                        >
+        <div style={{alignItems:"center", alignSelf:"center", alignContent:"center", marginLeft:window.innerWidth*0.01, boxShadow:10}}>
+          <h1 style={{fontWeight:"bolder", marginTop: 10, color:"#3d435a", fontFamily:"DM Sans"}}>BeachQueue</h1>
+          <p style={{ marginTop:50, color: "#3d435a", }}>Menu</p>
 
-    // Dashboard Component 
-    <Container className="d-flex flex-column py-2" style={{height: "100vh", fontFamily:"ui-rounded"}}>
-        <h1>Spotify Search Bar</h1>
-
-        <Form.Control
-            style={{margin:5}}
-            type="search"
-            placeholder="Search Songs/Artists"
-            onChange={(e)=>{setSearch(e.target.value)}}
-        />
-        <div style={{display:"flex", flexDirection:"row"}}>
-          <div>
-          {/* results component */}
-          <h1>Results</h1>
-          {searchResults.length === 0?
-            <div className="flex-grow-1 my-2" style={{ height: "75vh", overflowY: "auto", width: 700}}>
-              Search for a song in the search bar!
-            </div>
-            :
-            <DisplayResults trackList={searchResults} filterArr={goodSongsArr} />}
-          </div>
-          <div>
-          <h1>Next Up</h1>
-          <div style= {{ height: "30vh", overflowY: "auto"}}>
-            <Queue trackList={queueData} />
-          </div>
-          </div>
+          <Tabs orientation="vertical" value={value} onChange={handleChange}>
+            <Tab icon={<HomeRoundedIcon/>} iconPosition="start" label="Home" value="0" style={{width: window.innerWidth*0.15, borderRadius: 10, color: "#3d435a"}} fullWidth/>
+            <Tab icon={<ScheduleRoundedIcon/>} iconPosition="start" label= "History" value="1" style={{width: window.innerWidth*0.15, borderRadius: 10, color: "#3d435a"}} fullWidth/>
+            <Tab icon={<CommentRoundedIcon/>} iconPosition="start" label="How To Use" value="2" style={{width: window.innerWidth*0.15, borderRadius: 10, color: "#3d435a"}} fullWidth/>
+          </Tabs>
+ 
         </div>
-    </Container> 
+      </Container>
+
+      <Container style={{ fontFamily:"DM Sans", marginLeft:20, marginTop:10}}>
+          <h1 style={{color:"#4e69ec"}}>Home</h1>
+          <TextField
+              style={{margin:5, backgroundColor:"#ffffff", width: window.innerWidth*0.325}}
+              type="search"
+              placeholder="Search Songs/Artists"
+              onChange={(e)=>{setSearch(e.target.value)}}
+          />
+
+          <div 
+          style={{display:"flex", flexDirection:"row"}}
+          >
+            <div>
+            {/* results component */}
+            {searchResults.length === 0?
+              <Container 
+              sx={{boxShadow:3}}
+              style={{ height: window.innerHeight*0.8, marginTop: 10, overflowY: "auto", width: window.innerWidth*0.33, backgroundColor:"#ffffff", padding:10, borderRadius:10, color: "#3d435a"}}>
+                Search for a song in the search bar!
+              </Container>
+              :
+              <DisplayResults trackList={searchResults} filterArr={goodSongsArr} />}
+            </div>
+          </div>
+      </Container>
+      <TableContainer sx={{boxShadow:3 }} style={{
+                                          borderRadius:10,
+                                          backgroundColor:'#ffffff',
+                                          height: window.innerHeight*0.89,
+                                          overflowY: "auto",
+                                          marginTop: 70,
+                                          marginRight:30,
+                                          width: window.innerWidth*2,
+                                          overflowX:"hidden",
+                                          fontFamily:"DM Sans"
+                                          }}>
+        <Table
+        style={{marginLeft:10, marginTop:10}}
+        sx={{
+              [`& .${tableCellClasses.root}`]: {
+              borderBottom: "none" }
+          }}
+        >
+          <TableRow style={{height:window.innerHeight*0.35}}>
+            <h2 style={{color:"#3d435a"}}>Now Playing</h2>
+          </TableRow>
+          <TableRow>
+              <h2 style={{color:"#3d435a"}}>Next Up</h2>
+              <Queue trackList={queueData} />
+          </TableRow>
+        </Table>
+      </TableContainer>
+    </div>
     )}
 
 export default Dashboard;
