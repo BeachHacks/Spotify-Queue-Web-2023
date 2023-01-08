b2c3ca31306d0a1efa76ed051cb618822507925c
Quanzer <quanforcollege@gmail.com>
2023-01-07 22:54:34 -0800

    ignore for macbook testing
    ignore

--- client-sq/src/components/Track.js
+++ client-sq/src/components/Track.js
@@ -53,16 +53,16 @@ export default function Track({ track, clickable, num }) {
             <div  style={{width: '.9vw', marginLeft: -window.innerWidth*0.007}}></div>
 
             {clickable==false?
-            <TableCell style={{ padding: "2.5vh .45vw",fontSize: window.innerHeight*0.017, fontWeight : "bold",fontFamily:"DM Sans",color: "#6d7285", 
+            <TableCell style={{ padding: "2.5vh .45vw",fontSize: window.innerHeight*0.018, fontWeight : "bold",fontFamily:"DM Sans",color: "#6d7285", 
             
           }}>{num+2}</TableCell> 
             :""}
 
               
               {/* image  */}
-              {clickable==false? <TableCell style={{ padding: ".7vh 1vw",width: window.innerWidth*0.01 }} align="left">
+              {clickable==false? <TableCell style={{ padding: ".7vh .9vw",width: window.innerWidth*0.01 }} align="left">
                 <img src={track.albumUrl} alt={track.title} style={{height : window.innerHeight*0.05, width:window.innerHeight*0.05}} />
-              </TableCell> :<TableCell style={{ padding: ".7vh 1vw",width: window.innerWidth*0.01 }} align="left">
+              </TableCell> :<TableCell style={{ padding: ".7vh .9vw",width: window.innerWidth*0.01 }} align="left">
                 <img src={track.albumUrl} alt={track.title} style={{height : window.innerHeight*0.065, width: window.innerHeight*0.065}} />
               </TableCell>   }
            
@@ -70,15 +70,15 @@ export default function Track({ track, clickable, num }) {
 
               <div  style={{ marginLeft: -window.innerWidth*0.007}}>
               {/* Title and Artist  */}
-              {clickable==false? <TableCell style={{ padding: ".7vh 1vw",width: window.innerWidth*0.3, fontFamily:"DM Sans", color:"#3d435a"}} align="left">
+              {clickable==false? <TableCell style={{ padding: ".7vh .9vw",width: window.innerWidth*0.3, fontFamily:"DM Sans", color:"#3d435a"}} align="left">
                 <div style={{ fontWeight : "bold", fontSize: window.innerHeight*0.018, letterSpacing: -window.innerWidth*0.00015}}>
                   {track.title}
                 </div>
-                <div style={{ fontWeight : 300, color: "#6d7285", fontSize: window.innerHeight*0.018, letterSpacing: -window.innerWidth*0.00015}}>
+                <div style={{ fontWeight : 300, color: "#6d7285", fontSize: window.innerHeight*0.015, letterSpacing: -window.innerWidth*0.00015}}>
                   {track.artist}
                 </div>
               </TableCell> : 
-              <TableCell style={{ padding: ".7vh 1vw",width: window.innerWidth*0.3, fontFamily:"DM Sans", color:"#3d435a", letterSpacing: -window.innerWidth*0.00015}} align="left">
+              <TableCell style={{ padding: ".7vh .9vw",width: window.innerWidth*0.3, fontFamily:"DM Sans", color:"#3d435a", letterSpacing: -window.innerWidth*0.00015}} align="left">
                 <div style={{ fontWeight : "bold", fontSize : window.innerHeight*0.02}}>
                   {track.title}
                 </div>
@@ -91,7 +91,7 @@ export default function Track({ track, clickable, num }) {
           
 
               {/* Button Add to Queue */}
-              <TableCell style={{ padding: ".7vh 1vw"}} align="right">
+              <TableCell style={{ padding: ".7vh .9vw"}} align="right">
               {
                  !unqueueable && clickable? 
                  <IconButton onClick={handleAdd} style={{  marginRight: -window.innerWidth*0.008}} >
